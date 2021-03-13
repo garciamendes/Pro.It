5ac9317b73befc5d4da728f2196b141e29cbc9c6
garciamendes <garciaprog0101@gmail.com>
2021-03-13 20:00:32 -0300

    base backend, with user creation, login, listing and deletion

--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -3,13 +3,35 @@
 const User = use('App/Models/User')
 
 class UserController {
-  
-  async register({ request }) {
-    const data = request.only([ 'username', 'email', 'password' ])
 
-    const user = User.create(data)
+  async register({request}) {
+    const data = await request.only(['username', 'email', 'password'])
 
-    return user
+    const userCrate = User.create(data)
+
+    return userCrate
+  }
+
+  async login({request, auth}) {
+    const { email, password } = request.all()
+
+    const token = await auth.attempt(email, password)
+
+    return token
+  }
+
+  async list() {
+    const listUser = await User.all()
+    
+    return listUser
+  }
+
+  async deleteUser({params}) {
+    const user = await User.findOrFail(params.id)
+
+    const userDeleted = await user.delete()
+
+    return userDeleted
   }
 
 }
